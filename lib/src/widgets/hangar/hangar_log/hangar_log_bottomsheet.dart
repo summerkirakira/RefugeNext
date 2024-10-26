d80501afb113aace8f30fd61073a1e3b06e976e8
Hongpei Zheng <zhenghongpei@gmail.com>
2024-10-27 00:02:52 +0100

    🔖 发布4.1.3

--- lib/src/widgets/hangar/hangar_log/hangar_log_bottomsheet.dart
+++ lib/src/widgets/hangar/hangar_log/hangar_log_bottomsheet.dart
@@ -1,4 +1,5 @@
 import 'package:provider/provider.dart';
+import 'package:refuge_next/src/funcs/toast.dart';
 import 'package:refuge_next/src/widgets/hangar/hangar_log/hangar_log_page.dart';
 import 'package:wolt_modal_sheet/wolt_modal_sheet.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,8 @@ import 'package:flutter/material.dart';
 import '../../../datasource/data_model.dart';
 
 WoltModalSheetPage getHangarLogBottomSheet(BuildContext context, int? targetItemId) {
+  Provider.of<MainDataModel>(context, listen: false).refreshHangarLogs();
+  // showToast(message: "正在刷新机库日志");
   return WoltModalSheetPage(
     navBarHeight: 50,
     pageTitle: const Padding(
