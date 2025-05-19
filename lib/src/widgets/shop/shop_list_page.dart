aa331eb6c1efff8b0d42b21234bcb6b72dc2726a
Hongpei Zheng <zhenghongpei@gmail.com>
2025-05-19 22:26:06 +0100

    🎨 让商店页面支持刷新

--- lib/src/widgets/shop/shop_list_page.dart
+++ lib/src/widgets/shop/shop_list_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_custom_tab_bar/library.dart';
 import 'package:flutter/material.dart';
-import 'package:refuge_next/src/widgets/hangar/hangar_page.dart';
+import 'package:provider/provider.dart';
+
+import '../../datasource/data_model.dart';
 
 
 class ShopListPage extends StatefulWidget {
@@ -23,8 +25,51 @@ class _ShopListPageState extends State<ShopListPage> {
   @override
   void initState() {
     super.initState();
+    
+    // 添加页面切换监听
+    _tabBarController.addListener(() {
+      switch (_tabBarController.currentIndex) {
+        case 0: // 舰船
+          context.read<MainDataModel>().updateActivePageIndex(100);
+          break;
+        case 1: // 涂装
+          context.read<MainDataModel>().updateActivePageIndex(101);
+          break;
+        case 2: // 装备
+          context.read<MainDataModel>().updateActivePageIndex(102);
+          break;
+        case 3: // 订阅
+          context.read<MainDataModel>().updateActivePageIndex(103);
+          break;
+        case 4: // 游戏包
+          context.read<MainDataModel>().updateActivePageIndex(104);
+          break;
+        case 5: // 附加
+          context.read<MainDataModel>().updateActivePageIndex(105);
+          break;
+        case 6: // 组合包
+          context.read<MainDataModel>().updateActivePageIndex(106);
+          break;
+        case 7: // UEC
+          context.read<MainDataModel>().updateActivePageIndex(107);
+          break;
+        case 8: // 礼品卡
+          context.read<MainDataModel>().updateActivePageIndex(108);
+          break;
+        case 9: // 组合包
+          context.read<MainDataModel>().updateActivePageIndex(109);
+          break;
+        default:
+          break;
+      }
+    });
   }
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   Widget getTabbarChild(BuildContext context, int index) {
     return TabBarItem(
--- lib/src/widgets/shop/shop_top_bar.dart
+++ lib/src/widgets/shop/shop_top_bar.dart
@@ -10,7 +10,12 @@ import 'upgrade_shop/upgrade_ship_widget.dart';
 import 'package:badges/badges.dart' as badges;
 
 class ShopTopBar extends StatefulWidget {
-  const ShopTopBar({Key? key}) : super(key: key);
+  final List<GlobalKey<RefreshIndicatorState>>? refreshKeys;
+  
+  const ShopTopBar({
+    Key? key,
+    this.refreshKeys,
+  }) : super(key: key);
 
   @override
   _ShopTopBarState createState() => _ShopTopBarState();
@@ -54,6 +59,17 @@ class _ShopTopBarState extends State<ShopTopBar> {
                   )),
               const Text('商店', style: TextStyle(fontSize: 24)),
               const Spacer(),
+              if (widget.refreshKeys != null)
+                IconButton(
+                  onPressed: () {
+                    final activePageIndex = context.read<MainDataModel>().activePageIndex;
+                    if (activePageIndex >= 100 && activePageIndex < 110) {
+                      final index = activePageIndex - 100;
+                      widget.refreshKeys?[index].currentState?.show();
+                    }
+                  },
+                  icon: const Icon(Icons.refresh),
+                ),
               GestureDetector(
                 onTap: () async {
                   await onPressUpgrade(context);
